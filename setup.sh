21a324faf929f958705d97804b7ef6f9d3d66717
indra-ehep <77208636+indra-ehep@users.noreply.github.com>
2024-07-01 00:44:28 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,6 @@ CURRDIR="$PWD"
 cd /tmp/$USER
 git clone https://gitlab.cern.ch/pdauncey/hgcal10glinkreceiver.git
 cd $CURRDIR
-rsync -avP /tmp/idas/hgcal10glinkreceiver/common .
+rsync -avP /tmp/$USER/hgcal10glinkreceiver/common .
 ln -s /eos/cms/store/group/dpg_hgcal/tb_hgcal/2023/BeamTestSep/HgcalBeamtestSep2023 dat
 
