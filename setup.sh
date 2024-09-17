d6667e984577711eb5ca0d4b6357a05108b39a4c
Adinda de Wit <adinda.maite.de.wit@cern.ch>
2024-09-17 17:28:07 +0200

    Change setup script to relink if needed

--- setup.sh
+++ setup.sh
@@ -31,11 +31,13 @@ cd $CURRDIR
 rsync -avP $downloaddir/hgcal10glinkreceiver/common .
 #rsync -avP $downloadir/slow_control_configuration cfgmap/
 
-if [ ! -L dat ] ; then
-    if [ $islxplus -eq 1 ] ; then
-	ln -s /eos/cms/store/group/dpg_hgcal/tb_hgcal/2024/BeamTestAug/HgcalBeamtestAug2024 dat
-    fi
-    if [ $ishgcbeamtestpc -eq 1 ] ;  then
-	ln -s /till/HgcalBeamtest2024_TEST dat
-    fi
+if [ -L dat ] ; then
+    unlink dat
+fi
+
+if [ $islxplus -eq 1 ] ; then
+    ln -s /eos/cms/store/group/dpg_hgcal/tb_hgcal/2024/BeamTestAug/HgcalBeamtestAug2024 dat
+fi
+if [ $ishgcbeamtestpc -eq 1 ] ;  then
+    ln -s /till/HgcalBeamtest2024_TEST dat
 fi
