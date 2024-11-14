ea40cd577fe75fb2b5a6ad91d2db3d6f24ba8085
indra-ehep <indra.ehep@gmail.com>
2024-11-14 07:12:22 +0000

    code to analyze the decayed particles from tau

--- test-stage2_Nov24/ntupleMCInfo.cpp
+++ test-stage2_Nov24/ntupleMCInfo.cpp
@@ -23,6 +23,11 @@
 
 int main(int argc, char** argv)
 {
+
+  typedef struct{
+    std::string name;
+    int index;
+  }JetPart;
   
   bool doPrint = 1;
   std::cout << "========= Run as : ./ntupleMCInfo.exe $input_file $nofevents ==========" << std::endl;
@@ -84,7 +89,6 @@ int main(int argc, char** argv)
   std::vector<std::vector<int>>  *gen_daughters = 0 ;
   tr->SetBranchStatus("gen_daughters",1);
   tr->SetBranchAddress("gen_daughters" , &gen_daughters);
-
   
   std::vector<float>  *genpart_pt = 0 ;
   tr->SetBranchStatus("genpart_pt",1);
@@ -126,6 +130,10 @@ int main(int argc, char** argv)
   tr->SetBranchStatus("genpart_exphi",1);
   tr->SetBranchAddress("genpart_exphi" , &genpart_exphi);
 
+  Int_t genjet_n = 0 ;
+  tr->SetBranchStatus("genjet_n",1);
+  tr->SetBranchAddress("genjet_n" , &genjet_n);
+
   std::vector<float>  *genjet_pt = 0 ;
   tr->SetBranchStatus("genjet_pt",1);
   tr->SetBranchAddress("genjet_pt" , &genjet_pt);
@@ -137,6 +145,10 @@ int main(int argc, char** argv)
   std::vector<float>  *genjet_phi = 0 ;
   tr->SetBranchStatus("genjet_phi",1);
   tr->SetBranchAddress("genjet_phi" , &genjet_phi);
+
+  std::vector<float>  *genjet_energy = 0 ;
+  tr->SetBranchStatus("genjet_energy",1);
+  tr->SetBranchAddress("genjet_energy" , &genjet_energy);
   
   uint64_t totalEntries = tr->GetEntries();
   if(nofEvents==0) nofEvents = totalEntries;
@@ -149,38 +161,83 @@ int main(int argc, char** argv)
     
     tr->GetEntry(ievent) ;
     
-    if(doPrint) std::cout<<"Event : "<< ievent <<", nof gen : "<< gen_n << ", nof genpart : "<< genpart_pt->size() << std::endl;
+    if(doPrint) std::cout<<"Event : "<< ievent <<", nof gen : "<< gen_n << ", nof genpart : "<< genpart_pt->size() << ", njets : " << genjet_n << std::endl;
 
-    
+    std::vector<int> taudlist,taugdlist;
     for(int igen=0; igen<gen_n; igen++ ){
       TParticlePDG *partPDG = TDatabasePDG::Instance()->GetParticle(gen_pdgid->at(igen));
+      std::string gennom = (!partPDG)?"unknown":partPDG->GetName() ;
       std::string daughterlist = "(";
-      for(int const& idx: gen_daughters->at(igen)) { daughterlist += std::to_string(idx); daughterlist += ",";}
+      for(int const& idx: gen_daughters->at(igen)) {
+	daughterlist += std::to_string(idx);
+	daughterlist += ",";
+	if(gennom.find("tau")!=std::string::npos) taudlist.push_back(idx);
+	if(taudlist.size()>0 and std::find(taudlist.begin(),taudlist.end(),igen)!=taudlist.end()) taugdlist.push_back(idx);
+      }
       daughterlist += ")";
-      std::cout << "ievent: " << std::setprecision(default_precision) << std::setw(4) <<ievent << ", igen: " << std::setw(4) << igen
-    		<<", pid: " << std::setw(5) << gen_pdgid->at(igen) << ", status: " << std::setw(5) << gen_status->at(igen)
-    		<<", Name: " << std::setw(10) << ((!partPDG)?"unknown":partPDG->GetName())
-    		<<" (pt,eta,phi,energy) : (" << std::fixed << std::setprecision(2) << std::setw(10) << gen_pt->at(igen)
-		<< ", " << std::setw(10) << gen_eta->at(igen) << ", " << std::setw(10) << (TMath::RadToDeg()*gen_phi->at(igen)) << ", " << std::setw(10) << gen_energy->at(igen) << ") "
-		<<" daughters: " << (gen_daughters->at(igen)).size()
-		<< ", " << daughterlist 
-    		<< std::defaultfloat
-    		<< std::endl;
+      
+      if(gennom.find("h0")!=std::string::npos or gennom.find("tau")!=std::string::npos or (taudlist.size()>0 and std::find(taudlist.begin(),taudlist.end(),igen)!=taudlist.end()) or (taugdlist.size()>0 and std::find(taugdlist.begin(),taugdlist.end(),igen)!=taugdlist.end()))
+	std::cout << "ievent: " << std::setprecision(default_precision) << std::setw(4) <<ievent << ", igen: " << std::setw(4) << igen
+		  <<", pid: " << std::setw(5) << gen_pdgid->at(igen) << ", status: " << std::setw(5) << gen_status->at(igen)
+		  <<", Name: " << std::setw(10) << ((!partPDG)?"unknown":partPDG->GetName())
+		  <<" (pt,eta,phi,energy) : (" << std::fixed << std::setprecision(2) << std::setw(10) << gen_pt->at(igen)
+		  << ", " << std::setw(10) << gen_eta->at(igen) << ", " << std::setw(10) << (TMath::RadToDeg()*gen_phi->at(igen)) << ", " << std::setw(10) << gen_energy->at(igen) << ") "
+		  <<" daughters: " << (gen_daughters->at(igen)).size()
+		  << ", " << daughterlist 
+		  << std::defaultfloat
+		  << std::endl;
     }
     
+    std::vector<JetPart> partlist;
     for(int ipart=0; ipart<genpart_pt->size(); ipart++ ){
       TParticlePDG *partPDG = TDatabasePDG::Instance()->GetParticle(genpart_pid->at(ipart));
-      std::cout << "ievent: " << std::setprecision(default_precision) << std::setw(4) <<ievent << ", ipart: " << std::setw(4) << ipart
-    		<<", pid: " << std::setw(5) << genpart_pid->at(ipart) << ", mother: " << std::setw(4) << genpart_mother->at(ipart)
-    		<<", gen: " << std::setw(4) << (genpart_gen->at(ipart)-1) << ", fromBeamPipe: " << std::setw(4) << genpart_fromBeamPipe->at(ipart)
-    		<<", Name: " << std::setw(10) << ((!partPDG)?"unknown":partPDG->GetName())
-    		<<" (pt,eta,exeta,phi,exphi,energy) : (" << std::fixed << std::setprecision(2) << std::setw(3) << genpart_pt->at(ipart)
-		<< ", " << std::setw(5) << genpart_eta->at(ipart) << ", " << std::setw(5) << genpart_exeta->at(ipart)
-		<< ", " << std::setw(5) << (TMath::RadToDeg()*genpart_phi->at(ipart)) << ", " << std::setw(5) << (TMath::RadToDeg()*genpart_exphi->at(ipart))
-		<< ", " << std::setw(5) << genpart_energy->at(ipart) << ") "
-    		<< std::defaultfloat
-    		<< std::endl;
+      if((taudlist.size()>0 and std::find(taudlist.begin(),taudlist.end(),(genpart_gen->at(ipart)-1))!=taudlist.end()) or (taugdlist.size()>0 and std::find(taugdlist.begin(),taugdlist.end(),(genpart_gen->at(ipart)-1))!=taugdlist.end())){
+	JetPart p;
+	p.name = (!partPDG)?"unknown":partPDG->GetName();
+	p.index = ipart;
+	partlist.push_back(p);
+	std::cout << "ievent: " << std::setprecision(default_precision) << std::setw(4) <<ievent << ", ipart: " << std::setw(4) << ipart
+		  <<", pid: " << std::setw(5) << genpart_pid->at(ipart) << ", mother: " << std::setw(4) << genpart_mother->at(ipart)
+		  <<", gen: " << std::setw(4) << (genpart_gen->at(ipart)-1) //<< ", fromBeamPipe: " << std::setw(4) << genpart_fromBeamPipe->at(ipart)
+		  <<", Name: " << std::setw(10) << ((!partPDG)?"unknown":partPDG->GetName())
+		  <<" (pt,eta,phi,energy) : (" << std::fixed << std::setprecision(2) << std::setw(8) << genpart_pt->at(ipart)
+		  << ", " << std::setw(8) << genpart_eta->at(ipart) << ", " //<< std::setw(5) << genpart_exeta->at(ipart)
+		  << ", " << std::setw(8) << (TMath::RadToDeg()*genpart_phi->at(ipart)) //<< ", " << std::setw(5) << (TMath::RadToDeg()*genpart_exphi->at(ipart))
+		  << ", " << std::setw(8) << genpart_energy->at(ipart) << ") "
+		  << std::defaultfloat
+		  << std::endl;
+      }
     }
+    
+    std::vector<JetPart> jetlist;
+    for(int ijet=0; ijet<genjet_n; ijet++ ){
+      float minDeltaR = 1.0; int minDeltaRindex = -1;
+      for(int ipart=0; ipart<partlist.size(); ipart++){
+	int refpart = partlist.at(ipart).index;
+    	double deltaR = TMath::Sqrt((genpart_eta->at(refpart)-genjet_eta->at(ijet))*(genpart_eta->at(refpart)-genjet_eta->at(ijet)) + (genpart_phi->at(refpart)-genjet_phi->at(ijet))*(genpart_phi->at(refpart)-genjet_phi->at(ijet)));
+	if(deltaR<0.3 and deltaR<minDeltaR){
+	  minDeltaR = deltaR;
+	  minDeltaRindex = ipart;
+	}
+      }
+      for(int ipart=0; ipart<partlist.size(); ipart++){
+	JetPart p = partlist.at(ipart);
+	if(ipart==minDeltaRindex){
+	  JetPart jet;
+	  jet.name = p.name;
+	  jet.index = ijet ;
+	  jetlist.push_back(jet);
+	  std::cout << "ievent: " << std::setprecision(default_precision) << std::setw(4) <<ievent << ", ijet: " << std::setw(4) << ijet
+		    <<", Name: " << std::setw(10) << p.name << ", deltaR : " << std::setprecision(3) << std::setw(8) << minDeltaR
+		    <<" (pt,eta,phi,energy) : (" << std::fixed << std::setprecision(2) << std::setw(8) << genjet_pt->at(ijet)
+		    << ", " << std::setw(8) << genjet_eta->at(ijet) << ", " //<< std::setw(5) << genjet_exeta->at(ijet)
+		    << ", " << std::setw(8) << (TMath::RadToDeg()*genjet_phi->at(ijet)) //<< ", " << std::setw(5) << (TMath::RadToDeg()*genjet_exphi->at(ijet))
+		    << ", " << std::setw(8) << genjet_energy->at(ijet) << ") "
+		    << std::defaultfloat
+		    << std::endl;
+	}
+      }//part condition
+    }//jet loop
     
     gen_pt->clear();
     gen_eta->clear();
