99f5455f54a212a8a42f32e529802cde246e2132
Emyr Clement <emyr.clement@bristol.ac.uk>
2025-01-24 12:15:51 +0000

    Get both EMP tools and Stage2 emulation code.

--- EMPTools/getCMSSWCode.sh
+++ EMPTools/getCMSSWCode.sh
@@ -3,16 +3,25 @@ mkdir -p CMSSWCode
 cd CMSSWCode
 git init
 
-BRANCH=hgc-tpg-devel-CMSSW_14_0_0_pre1
-
-git remote add -t $BRANCH hgc-tpg git@github.com:hgc-tpg/cmssw.git
+BRANCH=S2Emu_standalone_Nov24
+REMOTE=emyr-cmssw
+echo "Fetching $BRANCH from $REMOTE"
+git remote add -t $BRANCH $REMOTE git@github.com:EmyrClement/cmssw.git
 
 git config core.sparseCheckout true
+
+# Stage 2 emulation
+echo L1Trigger/L1THGCal/interface/backend_emulator >> .git/info/sparse-checkout
+echo L1Trigger/L1THGCal/src/backend_emulator >> .git/info/sparse-checkout
+echo DataFormats/L1TParticleFlow/interface >> .git/info/sparse-checkout
+echo DataFormats/L1THGCal/interface >> .git/info/sparse-checkout
+
+# EMP Tools
 export BASE_URL=L1Trigger/DemonstratorTools
 for f in utilities Frame FileFormat BoardData; do
     echo $BASE_URL/interface/$f.h >> .git/info/sparse-checkout
     echo $BASE_URL/src/$f.cc >> .git/info/sparse-checkout
 done
-git fetch hgc-tpg $BRANCH
+git fetch $REMOTE $BRANCH
 git checkout -b $BRANCH FETCH_HEAD
 cd -
